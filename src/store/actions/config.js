c24c00dfce322eb3e09ffaa9b86316b5a4a2f6eb
Jeff Meyerson <meff.jeyerson@gmail.com>
2017-10-17 22:02:49 -0700

    changed endpoint to fix misspelling

--- src/store/actions/config.js
+++ src/store/actions/config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  'BASE_URL': 'https://software-enginnering-daily-api.herokuapp.com/api'
+  'BASE_URL': 'https://software-engineering-daily-api.herokuapp.com/api'
   // 'BASE_URL': 'http://localhost:4040/api'
 }
