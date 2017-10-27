3250e1e7507f9d48f68eb65eaee1b12c98a23800
jason <bjason@gmail.com>
2017-10-26 18:03:22 -0700

    Timeout refactor for tests

--- test/helpers/auth.test-helper.js
+++ test/helpers/auth.test-helper.js
@@ -4,28 +4,29 @@ const {Builder, By, until, promise} = webdriver;
 const seconds = 1000;
 const minutes = 60 * seconds;
 
+const timeout = 1 * minutes;
 
 // TODO: add comments
 async function register(driver, username, password) {
   // Click on the register nav button to load the registraion form:
-  const $registerLink = await driver.wait(until.elementLocated(By.name('register-nav-link')), 5 * minutes);
+  const $registerLink = await driver.wait(until.elementLocated(By.name('register-nav-link')), timeout);
   await $registerLink.click();
 
   // Fill in the registration form:
-  const $usernameInput = await driver.wait(until.elementLocated(By.name('username-input-field')), 5 * minutes);
+  const $usernameInput = await driver.wait(until.elementLocated(By.name('username-input-field')), timeout);
   await $usernameInput.sendKeys(username);
 
   // Fill in password
-  const $passwordInput = await driver.wait(until.elementLocated(By.name('password-input-field')), 5 * minutes);
+  const $passwordInput = await driver.wait(until.elementLocated(By.name('password-input-field')), timeout);
   await $passwordInput.sendKeys(password);
 
   // Submit form;
-  const $submitButton = await driver.wait(until.elementLocated(By.name('submit-button')), 5 * minutes);
+  const $submitButton = await driver.wait(until.elementLocated(By.name('submit-button')), timeout);
   await $submitButton.click();
 };
 
 async function login(driver, username, password) {
-  const $loginLink = await driver.wait(until.elementLocated(By.name('login-nav-link')), 5 * minutes);
+  const $loginLink = await driver.wait(until.elementLocated(By.name('login-nav-link')), timeout);
   await $loginLink.click();
 };
 
