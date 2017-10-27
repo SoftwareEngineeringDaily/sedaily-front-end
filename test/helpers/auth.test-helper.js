363690aa9358845a032656eecad77242a1dfc0b5
jason <bjason@gmail.com>
2017-10-26 18:06:38 -0700

    Updating test

--- test/helpers/auth.test-helper.js
+++ test/helpers/auth.test-helper.js
@@ -7,18 +7,22 @@ const minutes = 60 * seconds;
 const timeout = 1 * minutes;
 
 // TODO: add comments
-async function register(driver, username, password) {
+async function register(driver, {username, name, password}) {
   // Click on the register nav button to load the registraion form:
   const $registerLink = await driver.wait(until.elementLocated(By.name('register-nav-link')), timeout);
   await $registerLink.click();
 
   // Fill in the registration form:
-  const $usernameInput = await driver.wait(until.elementLocated(By.name('username-input-field')), timeout);
+  const $usernameInput = await driver.wait(until.elementLocated(By.name('username')), timeout);
   await $usernameInput.sendKeys(username);
 
   // Fill in password
-  const $passwordInput = await driver.wait(until.elementLocated(By.name('password-input-field')), timeout);
+  const $passwordInput = await driver.wait(until.elementLocated(By.name('password')), timeout);
   await $passwordInput.sendKeys(password);
+
+  // Fill in name
+  const $nameInput  = await driver.wait(until.elementLocated(By.name('name')), timeout);
+  await $nameInput.sendKeys(name);
 
   // Submit form;
   const $submitButton = await driver.wait(until.elementLocated(By.name('submit-button')), timeout);
