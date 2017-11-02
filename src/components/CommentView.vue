a8373c09704d1a7446f3dc3cfc462590c7937eb3
jason <bjason@gmail.com>
2017-11-01 17:14:26 -0700

    Refactoring comments

---
+++ src/components/CommentView.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class='comment'>
+    <span class='content'>
+      <div>
+        {{username(comment)}}  <span class='comment-date'> {{date(comment)}} </span>
+      </div>
+      {{comment.content}}
+    </span>
+    <hr />
+  </div>
+</template>
+
+
+<script>
+/* @flow */
+import moment from 'moment'
+import { mapState } from 'vuex'
+export default {
+  name: 'comments-list',
+  props: ['comment'],
+  computed: {
+    ...mapState({
+      isRootLevelComment() {
+        return !this.comment.parentComment 
+      }
+      me (state) {
+        return state.me
+      }
+    })
+  },
+  methods: {
+    username (comment: {content: string, dateCreated: string, author: {name: string} }) {
+      if (comment.author) {
+        return comment.author.name
+      } else {
+        // Means we just made this comment
+        return this.me.name
+      }
+    },
+    date (comment: {content: string, dateCreated: string, author: {name: string} }) {
+      if (comment.dateCreated) {
+        return moment(comment.dateCreated).format('LL')
+      } else {
+        return 'Now'
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+.comment-date {
+  padding-left: 10px;
+  color: #ccc;
+}
+.comment {
+  display: flex;
+}
+.profile-img {
+  width: 80px;
+  height: 80px;
+}
+.content {
+  margin-left: 20px;
+}
+</style>
--- src/components/CommentsList.vue
+++ src/components/CommentsList.vue
@@ -1,15 +1,6 @@
 <template>
   <div>
     <div v-for="comment in comments">
-      <div class='comment'>
-        <span class='content'>
-          <div>
-            {{username(comment)}}  <span class='comment-date'> {{date(comment)}} </span>
-          </div>
-          {{comment.content}}
-        </span>
-      </div>
-      <hr />
     </div>
   </div>
 </template>
@@ -21,30 +12,6 @@ import { mapState } from 'vuex'
 export default {
   name: 'comments-list',
   props: ['comments'],
-  computed: {
-    ...mapState({
-      me (state) {
-        return state.me
-      }
-    })
-  },
-  methods: {
-    username (comment: {content: string, dateCreated: string, author: {name: string} }) {
-      if (comment.author) {
-        return comment.author.name
-      } else {
-        // Means we just made this comment
-        return this.me.name
-      }
-    },
-    date (comment: {content: string, dateCreated: string, author: {name: string} }) {
-      if (comment.dateCreated) {
-        return moment(comment.dateCreated).format('LL')
-      } else {
-        return 'Now'
-      }
-    }
-  }
 }
 </script>
 
