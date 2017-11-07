18d7c8894c8e373a0b78f96590c5c58674dca702
jason <bjason@gmail.com>
2017-11-07 11:58:14 -0800

    Staging and production in config

--- src/store/actions/config.js
+++ src/store/actions/config.js
@@ -1,4 +1,12 @@
 module.exports = {
-  'BASE_URL': 'https://software-enginnering-daily-api.herokuapp.com/api'
-  // 'BASE_URL': 'http://localhost:4040/api'
+  // PRODUCTION
+  // 'BASE_URL': 'https://software-enginnering-daily-api.herokuapp.com/api'
+  'BASE_URL': 'http://localhost:4040/api'
+
+  // STAGING
+  // 'BASE_URL': 'https://sedaily-backend-staging.herokuapp.com/api/'
+
 }
+
+// FAKE STAGING
+// 'BASE_URL': 'https://sedailystaging-fake.herokuapp.com/api'
