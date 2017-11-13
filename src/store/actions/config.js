bd1f1f6cd639be25f931b0ac0104a71e1891f718
jason <bjason@gmail.com>
2017-11-13 14:30:18 -0800

    Clean up config file

--- src/store/actions/config.js
+++ src/store/actions/config.js
@@ -1,12 +1,10 @@
 module.exports = {
   // PRODUCTION
   'BASE_URL': 'https://software-enginnering-daily-api.herokuapp.com/api'
+
+  // LOCAL
   // 'BASE_URL': 'http://localhost:4040/api'
 
   // STAGING
   // 'BASE_URL': 'https://sedaily-backend-staging.herokuapp.com/api/'
-
 }
-
-// FAKE STAGING
-// 'BASE_URL': 'https://sedailystaging-fake.herokuapp.com/api'
