07d30968c8ed510a2c116f7d1c75e96bab50f42a
jason <bjason@gmail.com>
2017-11-16 15:01:18 -0800

    Feed view

---
+++ src/views/FeedView.vue
@@ -0,0 +1,41 @@
+
+<template>
+  <div>
+    <br />
+    <br />
+  </div>
+</template>
+
+<script>
+import { mapActions, mapState } from 'vuex'
+export default {
+  name: 'feed-view',
+  data () {
+    return {
+      loading: true
+    }
+  },
+  beforeMount () {
+    this.fetchMyProfileData()
+    .then(() => {
+      this.loading = false
+      // this.fetchMyFeed
+      // this.username = this.me.username
+    })
+  },
+  methods: {
+    ...mapActions([ 'fetchMyProfileData' ])
+  },
+
+  computed: {
+    ...mapState({
+      me (state) {
+        return state.me
+      }
+    })
+  }
+}
+</script>
+
+<style scoped>
+</style>
