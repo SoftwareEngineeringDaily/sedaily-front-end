32c7741a1452983afa01b64a9f59012c2e7f034d
jason <bjason@gmail.com>
2017-11-17 10:55:48 -0800

    Fetch my feed

--- src/views/FeedView.vue
+++ src/views/FeedView.vue
@@ -2,6 +2,23 @@
 <template>
   <div>
     <br />
+        <h1>My Feed </h1>
+        <br />
+        <br />
+        <div v-for="feedItem in feed">
+          <a :href="feedItem.url" target="_blank"
+          rel="external nofollow">
+          <img class="feed-item-img":src="image(feedItem.image)"  />
+           <span class='title'>
+           {{feedItem.title || feedItem.url}}
+           </span>
+         </a>
+         <br />
+           <p class='votes'>
+           Votes:{{feedItem.score}}
+           </p>
+          <hr />
+        </div>
     <br />
   </div>
 </template>
@@ -15,22 +32,32 @@ export default {
       loading: true
     }
   },
+
   beforeMount () {
     this.fetchMyProfileData()
     .then(() => {
-      this.loading = false
-      // this.fetchMyFeed
-      // this.username = this.me.username
+      this.fetchMyFeed({userId: this.me._id})
+        .then((feedItems) => {
+          console.log(feedItems)
+          this.loading = false
+        })
     })
   },
   methods: {
-    ...mapActions([ 'fetchMyProfileData' ])
+    ...mapActions([ 'fetchMyProfileData', 'fetchMyFeed' ]),
+    image (image) {
+      console.log(image)
+      return image !== undefined ? image : 'https://s3-us-west-2.amazonaws.com/sd-profile-pictures/linkplaceholder.png'
+    }
   },
 
   computed: {
     ...mapState({
       me (state) {
         return state.me
+      },
+      feed (state) {
+        return state.feed
       }
     })
   }
@@ -38,4 +65,13 @@ export default {
 </script>
 
 <style scoped>
+.title {
+  margin-left: 10px;
+}
+.votes {
+  padding-top: 5px;
+}
+.feed-item-img {
+  width:  100px;
+}
 </style>
