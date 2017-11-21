e8d629164f17a9bb7a10d22ed32f7a69d09884bc
jason <bjason@gmail.com>
2017-11-21 13:34:14 -0800

    Subscriptions Hello World

---
+++ src/views/SubscribeView.vue
@@ -0,0 +1,57 @@
+<template>
+  <div id='app'>
+    <h1>Please give us your payment details:</h1>
+    <card class='stripe-card'
+      :class='{ complete }'
+      stripe='pk_test_RayhhznsRXj6hqZ8SnKJY70Y'
+      :options='stripeOptions'
+      @change='complete = $event.complete'
+    />
+    <button class='pay-with-stripe' @click='pay' :disabled='!complete'>Pay with credit card</button>
+  </div>
+</template>
+
+<script>
+// import { stripeKey, stripeOptions } from './stripeConfig.json'
+import { Card, createToken } from 'vue-stripe-elements-plus'
+
+export default {
+  data () {
+    return {
+      complete: false,
+      stripeOptions: {
+        hidePostalCode: false
+        // see https://stripe.com/docs/stripe.js#element-options for details
+      }
+    }
+  },
+
+  components: { Card },
+
+  methods: {
+    pay () {
+      // createToken returns a Promise which resolves in a result object with
+      // either a token or an error key.
+      // See https://stripe.com/docs/api#tokens for the token object.
+      // See https://stripe.com/docs/api#errors for the error object.
+      // More general https://stripe.com/docs/stripe.js#stripe-create-token.
+      createToken().then(data => {
+        console.log(data.token)
+        console.log('token', data.token.id)
+        // We want : stripeToken tok_1BQj6ULmlLorKKLQ6xoRYTHR
+        console.log(data)
+      })
+    }
+  }
+}
+</script>
+
+<style>
+.stripe-card {
+  width: 300px;
+  border: 1px solid grey;
+}
+.stripe-card.complete {
+  border-color: green;
+}
+</style>
