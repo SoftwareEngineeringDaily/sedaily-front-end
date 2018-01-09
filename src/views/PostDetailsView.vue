53ae1b7abfb64922b68224e5cc2a4ea874e1c27a
jason <bjason@gmail.com>
2018-01-09 15:01:17 -0800

    Play button fixes

--- src/views/PostDetailsView.vue
+++ src/views/PostDetailsView.vue
@@ -26,14 +26,19 @@
           </span>
 
           <p class="meta">
-            {{ post.score || 0 }} points
-            <!-- | by <router-link :to="'/user/' + post.by">{{ post.by }}</router-link> -->
-            {{ post.date | timeAgo }} ago
-            <social-sharing :url="post.url"
-                      :title="post.title.rendered"
-                      :description="postContent"
-                      twitter-user="software_daily"
-                      inline-template>
+
+            <span class="play-button" @click='setActivePostInPlayer(post)'>
+              <img class="play-icon" src="../assets/play.png" alt="play">
+            </span>
+            <span class='details-about-post'>
+              {{ post.score || 0 }} points
+              <!-- | by <router-link :to="'/user/' + post.by">{{ post.by }}</router-link> -->
+              {{ post.date | timeAgo }} ago
+              <social-sharing :url="post.url"
+              :title="post.title.rendered"
+              :description="postContent"
+              twitter-user="software_daily"
+              inline-template>
               <div class="sharing">
                 <network network="facebook">
                   <i class="fa fa-facebook"></i> Facebook
@@ -46,6 +51,7 @@
                 </network>
               </div>
             </social-sharing>
+          </span>
           </p>
         </div>
       </div>
@@ -194,6 +200,10 @@ export default {
   methods: {
     ...mapActions(['commentsCreate', 'upvote', 'relatedLinksFetch',
       'downvote', 'fetchArticle', 'commentsFetch']),
+
+    setActivePostInPlayer: function (post:any) {
+      this.$store.commit('setActivePostInPlayer', { post })
+    },
     selectPostContent () {
       this.showRelatedLinks = false
       this.showComments = false
@@ -237,6 +247,20 @@ secondary-color = #FF8B6A
 .post-transcript {
 }
 
+.play-button
+
+  height 80px
+  top 40px
+  left 70px
+  cursor pointer
+
+  .play-icon
+    width 80px
+.details-about-post {
+  margin-top: 20px;
+  display: inline-block;
+}
+
 .selection-icons {
   margin-bottom: 30px;
   .icon {
@@ -257,7 +281,6 @@ secondary-color = #FF8B6A
 .post-view-header
   background primary-color
   color white
-  padding 1.8em 2em 1em
   margin-top 20px
   box-shadow 0 1px 2px rgba(0,0,0,.1)
 
