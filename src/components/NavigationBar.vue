38f3bcd54d62916bb7fb2347098f933d5c542a20
dfcook <dfcook@hotmail.com>
2018-01-14 09:23:15 +0000

    Keep Subscribe button consistently on the same place

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -1,42 +1,51 @@
 <template>
   <header class="header">
     <nav class="inner">
-      <router-link to="/" exact>
+      <router-link to="/"
+        exact>
         <img class="logo" src="./../assets/sedaily-logo.png" alt="logo">
       </router-link>
-      <router-link to="/feed"  name="feed-nav-link" exact   v-if='isLoggedIn'>
-        Feed
-      </router-link>
-      <router-link to="/new" name="new-nav-link">
-        New
-      </router-link>
-      <router-link to="/top" name="top-nav-link">
-        Top
-      </router-link>
-      <router-link to="/recommendations" name="recommendations-nav-link">
-        Recommended
-      </router-link>
 
-      <router-link to="/login" name="login-nav-link" style='float:right;' v-if='!isLoggedIn'>Login</router-link>
+      <router-link to="/feed"
+        name="feed-nav-link" 
+        exact 
+        v-if='isLoggedIn'>Feed</router-link>
 
-      <router-link to="/register" name="register-nav-link" style='float:right;margin-right: 1em;' v-if='!isLoggedIn'>
-        Register
-      </router-link>
+      <router-link to="/new" 
+        name="new-nav-link">New</router-link>
 
-      <router-link to="/premium" name="subscribe-nav-link"
-      style='float:right;margin-right: 1em;' v-if='!alreadySubscribed'
-        class="call-to-action"
-      >
-        Subscribe
-      </router-link>
+      <router-link to="/top" 
+        name="top-nav-link">Top</router-link>
 
-      <span v-if='isLoggedIn' style='float:right;'>
-        <router-link v-if='alreadySubscribed' to="/subscribe" name="top-nav-link"
-        class="subscribed"
-        >Subscribed</router-link>
+      <router-link to="/recommendations" 
+        name="recommendations-nav-link">Recommended</router-link>
 
-        <a href='/'   name="logouts-nav-link"  @click.prevent='logoutHandler'>Logout</a>
-        <router-link to="/profile" name="top-nav-link">Profile</router-link>
+      <span class="pull-right">
+        <span v-if="isLoggedIn">
+          <a href='/' 
+            name="logouts-nav-link"
+            @click.prevent='logoutHandler'>Logout</a>
+          <router-link to="/profile" 
+            name="top-nav-link">Profile</router-link>          
+        </span>
+        <span v-else>
+          <router-link to="/login" 
+            name="login-nav-link">Login</router-link>
+
+          <router-link to="/register" 
+            name="register-nav-link" 
+            class="register-nav-link">Register</router-link>
+        </span>   
+
+        <router-link v-if='alreadySubscribed' 
+          to="/subscribe" 
+          name="top-nav-link"
+          class="subscribed">Subscribed</router-link>
+
+        <router-link v-else
+          to="/premium" 
+          name="subscribe-nav-link"
+          class="call-to-action">Subscribe</router-link>  
       </span>
     </nav>
   </header>
@@ -85,8 +94,11 @@ export default {
 </script>
 
 <style scoped lang="stylus">
-
-.header .call-to-action
-  color white
-  text-decoration none
+.header 
+  .call-to-action
+    color white
+    text-decoration none    
+    margin-right 1em
+  .register-nav-link
+    margin-right 1em
 </style>
