30030dcd61535475db686ac0e58f0ea299c4f72c
dfcook <dfcook@hotmail.com>
2018-01-21 11:54:35 +0000

    Combine "New", "Top", "Recommended" Into 1 Dropdown

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -11,14 +11,31 @@
         exact
         >Feed</router-link>
 
-      <router-link to="/new"
-        name="new-nav-link">New</router-link>
-
-      <router-link to="/top"
-        name="top-nav-link">Top</router-link>
-
-      <router-link to="/recommendations"
-        name="recommendations-nav-link">Recommended</router-link>
+      <span class="dropdown">
+        <router-link to="/"
+          class="dropdown-toggle"
+          @mouseover.native="showDropdownMenu"
+          name="new-nav-link">Podcast</router-link>
+          <div class="dropdown-menu"
+            @mouseleave.stop="hideDropdownMenu"
+            :style="dropdownMenuStyle">
+            <router-link to="/new"
+              class="dropdown-item"
+              name="feed-nav-link"
+              exact
+              >New</router-link>
+            <router-link to="/top"
+              class="dropdown-item"
+              name="feed-nav-link"
+              exact
+              >Top</router-link>
+            <router-link to="/recommended"
+              class="dropdown-item"
+              name="feed-nav-link"
+              exact
+              >Recommended</router-link>
+          </div>
+      </span>
 
       <span class="pull-right">
         <span v-if="isLoggedIn">
@@ -57,6 +74,14 @@ import { mapActions, mapGetters, mapState } from 'vuex'
 export default {
   name: 'navigation-bar',
 
+  data () {
+    return {
+      dropdownMenuStyle: {
+        display: 'none'
+      }
+    }
+  },
+
   computed: {
     ...mapGetters(['isLoggedIn']),
     ...mapState({
@@ -88,6 +113,16 @@ export default {
     logoutHandler () {
       this.logout()
       this.$router.replace('/')
+    },
+    showDropdownMenu () {
+      this.dropdownMenuStyle = {
+        display: 'block'
+      }
+    },
+    hideDropdownMenu () {
+      this.dropdownMenuStyle = {
+        display: 'none'
+      }
     }
   }
 }
@@ -101,4 +136,6 @@ export default {
     margin-right 1em
   .register-nav-link
     margin-right 1em
+  a.router-link-active
+    text-decoration none
 </style>
