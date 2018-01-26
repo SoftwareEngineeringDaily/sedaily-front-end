606f737df2263762704e463f4dfae1570513a58c
Andrew Lloyd <andrew85.lloyd@gmail.com>
2018-01-26 12:28:37 -0800

    Add the logging.post actions for sending events to the events-api server.

--- src/store/actions/config.js
+++ src/store/actions/config.js
@@ -12,5 +12,8 @@ module.exports = {
   // 'STRIPE_PUBLIC_KEY': 'pk_test_vMQkcW5KspBBOQ0hn5nNshDm'
 
   // PRODUCTION:
-  'STRIPE_PUBLIC_KEY': 'pk_live_Cfttsv5i5ZG5IBfrmllzNoSA'
+  'STRIPE_PUBLIC_KEY': 'pk_live_Cfttsv5i5ZG5IBfrmllzNoSA',
+
+  // PRODUCTION:
+  'EVENTS_API_BASE_URL': 'http://ec2-54-201-88-214.us-west-2.compute.amazonaws.com:3000/api/v1/event'
 }
