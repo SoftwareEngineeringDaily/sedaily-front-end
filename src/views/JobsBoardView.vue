8146053a09a9345629660fb47ec02442b52fd49c
Josh Morel <morel.josh@hotmail.com>
2018-02-11 19:10:52 -0500

    WIP job search

--- src/views/JobsBoardView.vue
+++ src/views/JobsBoardView.vue
@@ -7,6 +7,13 @@
     <div class="row justify-content-center">
       <h1 class="col-10 text-center">Jobs Board</h1>
     </div>
+    <div class='search-bar'>
+      <input
+        class='search-bar-input'
+        type='text'
+        placeholder='Search...'
+        v-model='searchTerm'/>
+    </div>
     <div class="row justify-content-center add-job-link">
         <router-link class="col-10 text-center" v-if="isLoggedIn" to="/add-job">
           Employers: Post a Job
@@ -21,7 +28,7 @@
     <br>
     <div class="row">
       <div class="col-sm-8 offset-sm-2 col-md-6 offset-md-3">
-        <job-summary v-for="job in jobs" :key="job._id" :job="job">
+        <job-summary v-for="job in filteredJobs" :key="job._id" :job="job">
         </job-summary>
       </div>
     </div>
@@ -43,7 +50,8 @@ export default {
   data () {
     return {
       loading: false,
-      error: null
+      error: null,
+      searchTerm: ''
     }
   },
   beforeMount () {
@@ -65,9 +73,20 @@ export default {
 
   computed: {
     ...mapGetters(['isLoggedIn']),
+    // TODO: determine preferred searching method
     ...mapState({
       jobs (state) {
         return state.jobs
+      },
+      filteredJobs () {
+        if (this.searchTerm.trim() === '') {
+          return this.jobs
+        } else {
+          const searchRe = new RegExp(this.searchTerm.trim(), 'i')
+          return this.jobs.filter((job) => {
+            return job.title.search(searchRe) >= 0 || job.companyName.search(searchRe) >= 0 || job.location.search(searchRe) >= 0
+          })
+        }
       }
     })
   }
@@ -79,4 +98,16 @@ export default {
 .add-job-link
   a
     color accent-color
+
+.search-bar
+  input
+    width 100%
+    margin 20px 0
+    padding 10px
+    font-size 2rem
+    font-weight 100
+    color #C4C4C4
+    padding-left 20px
+    border none
+    border-bottom 1px solid #ccc
 </style>
