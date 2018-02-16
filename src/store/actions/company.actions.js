3dfcf6780cb777613424c3ff5219e8ba0e6414d4
jason <bjason@gmail.com>
2018-02-16 14:21:21 -0800

    Fetching companies

--- src/store/actions/company.actions.js
+++ src/store/actions/company.actions.js
@@ -3,7 +3,7 @@ import { apiConfig } from '../../../config/apiConfig'
 const BASE_URL = apiConfig.BASE_URL
 
 export default {
-  companisFetch ({ getters, commit }, { postId }) {
+  companiesFetch ({ getters, commit }) {
     const options = {}
     const token = getters.getToken
     if (token) {
@@ -12,7 +12,7 @@ export default {
       }
     }
 
-    const url = `${BASE_URL}/posts/companies`
+    const url = `${BASE_URL}/companies`
     return axios.get(url, options)
       .then((response) => {
         const companies = response.data
