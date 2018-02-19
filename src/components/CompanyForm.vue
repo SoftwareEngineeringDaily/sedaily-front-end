844f5aadf3fada5aaa1c8abb55223e395397a961
jason <bjason@gmail.com>
2018-02-18 16:54:40 -0800

    Company name

--- src/components/CompanyForm.vue
+++ src/components/CompanyForm.vue
@@ -9,13 +9,70 @@
           <input
           class="form-control"
           type="text"
-          placeholder="Company/Employer"
+          placeholder="Company Name"
           id="companyNameInput"
           name="companyName"
           v-model="companyFormData.companyName"
           >
         </div>
       </div>
+
+      <div class="form-group row">
+        <label for="companyNameInput" class="col-sm-2 col-form-label">Description</label>
+        <div class="col-sm-10">
+          <input
+          class="form-control"
+          type="text"
+          placeholder="Description"
+          id="descriptionInput"
+          name="description"
+          v-model="companyFormData.description"
+          >
+        </div>
+      </div>
+
+      <div class="form-group row">
+        <label for="companyNameInput" class="col-sm-2 col-form-label">Image Url:</label>
+        <div class="col-sm-10">
+          <input
+          class="form-control"
+          type="text"
+          placeholder="https://someurl.com/image.jpg"
+          id="imageUrlInput"
+          name="imageUrl"
+          v-model="companyFormData.imageUrl"
+          >
+        </div>
+      </div>
+
+      <div class="form-group row">
+        <label for="companyNameInput" class="col-sm-2 col-form-label">External Url:</label>
+        <div class="col-sm-10">
+          <input
+          class="form-control"
+          type="text"
+          placeholder="https://company.com"
+          id="externalUrlInput"
+          name="externalUrl"
+          v-model="companyFormData.externalUrl"
+          >
+        </div>
+      </div>
+
+      <div class="form-group row">
+        <label for="companyNameInput" class="col-sm-2 col-form-label">Local Url:</label>
+        <div class="col-sm-10">
+          <input
+          class="form-control"
+          type="text"
+          placeholder="some-company"
+          id="localUrlInput"
+          name="localUrl"
+          v-model="companyFormData.localUrl"
+          >
+        </div>
+      </div>
+
     </form>
 
   </div>
