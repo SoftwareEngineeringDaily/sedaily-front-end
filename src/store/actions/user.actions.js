e78712e738f85cfe21884e31ad7c59359b7583dd
jason <bjason@gmail.com>
2018-02-23 04:28:03 -0800

    Refactor image upload

--- src/store/actions/user.actions.js
+++ src/store/actions/user.actions.js
@@ -1,48 +1,14 @@
-
 import axios from 'axios'
 import { apiConfig } from '../../../config/apiConfig'
+import { getS3SingedUploadUrlAndUpload } from '../../utils/uploadImage.utils'
 const BASE_URL = apiConfig.BASE_URL
 
-const uploadFile = (file, signedRequest, url) => {
-  const p = new Promise((resolve, reject) => {
-    const xhr = new XMLHttpRequest()
-    xhr.open('PUT', signedRequest)
-    xhr.onreadystatechange = () => {
-      if (xhr.readyState === 4) {
-        if (xhr.status === 200) {
-          console.log('Success!!! url', url)
-          resolve({ imageUrl: url })
-        } else {
-          reject('Could not upload file.')
-        }
-      }
-    }
-    xhr.send(file)
-  })
-  return p
-}
-
 export default {
-  getS3SingedUploadUrl: ({ commit, state, getters }, { fileName, fileType }) => {
-  },
 
   uploadAvatarImage: ({ commit, state, getters }, { imageFile }) => {
     const token = getters.getToken
-    const config = {}
-    if (token) {
-      config.headers = {
-        'Authorization': 'Bearer ' + token
-      }
-    }
-
-    const fileType = imageFile.type
-    return axios.post(`${BASE_URL}/auth/sign-s3`, { fileType }, config)
-      .then((result) => {
-        const { signedRequest, url } = result.data
-        console.log('signedRequest', signedRequest)
-        // return axios.put(signedRequest, imageFile, options)
-        return uploadFile(imageFile, signedRequest, url)
-      })
+    const endpointUrl = `${BASE_URL}/auth/sign-s3`
+    getS3SingedUploadUrlAndUpload({ token, imageFile, endpointUrl })
   },
 
   fetchMyProfileData: ({ commit, state, getters }) => {
