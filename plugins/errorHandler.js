3c1f4494c6b9410875042da2c796b90227032997
dfcook <dfcook@hotmail.com>
2018-03-12 18:32:33 +0000

    Make toast visible until cleared and log errors to console

--- plugins/errorHandler.js
+++ plugins/errorHandler.js
@@ -1,6 +1,15 @@
 export default function ({ $axios, redirect, app, store }) {
   $axios.onError(error => {
-    // with @nuxtjs/toast
-    app.$toast.error('Error while making request: ' + error.message)
+    console.log(error)
+    app.$toast.error('Error while making request: ' + error.message, {
+      theme: 'primary',
+      duration: null,
+      action: {
+        text : 'Close',
+        onClick : (e, toastObject) => {
+            toastObject.goAway(0);
+        }
+      }
+    })
  })
 }
