bc21a3a5a76da79022da20106e69cb0fe3bc3942
jason <bjason@gmail.com>
2018-03-15 06:40:08 -0700

    Adding comments fetch to forum

--- src/views/ForumThreadView.vue
+++ src/views/ForumThreadView.vue
@@ -7,13 +7,35 @@
       <p>
         {{forumThread.content}}
       </p>
+
+
+      <div class="row">
+        <div class="col-md-12">
+          <comment-compose v-if="isLoggedIn"></comment-compose>
+        </div>
+      </div>
+      <br />
+      <br />
+      <div class="row">
+        <div class="col-md-12">
+          <h3 class='section-title'> Comments </h3>
+          <comments-list :comments='comments'></comments-list>
+        </div>
+
+            </div>
+    </div>
   </div>
   </div>
 </template>
 
 <script>
-import { mapState, mapActions } from 'vuex'
+import Spinner from '@/components/Spinner.vue'
+import CommentsList from '@/components/CommentsList.vue'
+import CommentCompose from '@/components/CommentCompose.vue'
+import { mapState, mapActions, mapGetters } from 'vuex'
 export default {
+  name: 'forum-thread-view',
+  components: { Spinner, CommentsList, CommentCompose },
   data () {
     return {
       loading: true
@@ -21,23 +43,37 @@ export default {
   },
 
   computed: {
+    ...mapGetters(['isLoggedIn']),
     forumThread () {
       return this.$store.state.forumThreads[this.$route.params.id]
     },
-
+    comments () {
+      return this.entityComments[this.$route.params.id] || []
+    },
     ...mapState({
+      entityId (state) {
+        return state.route.params.id
+      },
       threadId (state) {
         return state.route.params.id
+      },
+      entityComments (state) {
+        return state.entityComments
       }
     })
   },
 
   methods: {
-    ...mapActions(['fetchForumThread'])
+    ...mapActions(['fetchForumThread', 'commentsFetch'])
   },
   beforeMount () {
     this.fetchForumThread({
-      id: this.threadId
+      id: this.entityId
+    })
+
+    // Fetch comments
+    this.commentsFetch({
+      entityId: this.entityId
     })
   }
 }
