09effc7b2cd66750c102151c4b3a63eab1fac61e
jason <bjason@gmail.com>
2018-03-30 15:34:30 -0700

    Fix bug with replies showing Reply button

--- src/components/CommentView.vue
+++ src/components/CommentView.vue
@@ -22,10 +22,10 @@
 
         <span class='comment-date'> {{date(comment)}} </span>
 
-        <div class="bullet-point">&#9679;</div>
+        <div v-if="allowsReplies" class="bullet-point">&#9679;</div>
 
         <span v-if="!isReplying && isLoggedIn">
-          <span class='link' @click="isReplying=!isReplying">Reply</span>
+          <span v-if="allowsReplies" class='link' @click="isReplying=!isReplying">Reply</span>
         </span>
         <span v-else class='link' @click="isReplying=!isReplying">Cancel</span>
 
