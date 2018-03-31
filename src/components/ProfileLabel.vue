1dc5033e7f153e3aff540b7c97fa9bedf329b177
dfcook <dfcook@hotmail.com>
2018-03-31 08:25:23 +0100

    #348 Avatar aspect ratio kept when made into circles

--- src/components/ProfileLabel.vue
+++ src/components/ProfileLabel.vue
@@ -1,46 +1,57 @@
 <template>
   <span>
     <router-link :to="'/profile/' + userData._id">
-      <img :src='avatarUrl' class='avatar'/>
+      <div
+        :style="imageStyle"
+        class="avatar" >
+      </div>
     </router-link>
-    <span v-if="showName" class="name">{{displayName}}</span>
+
+    <span
+      v-if="showName"
+      class="name">{{displayName}}</span>
+
     <slot></slot>
   </span>
 </template>
 
 <script>
-  import { mapState } from 'vuex'
-  export default {
-    name: 'profile-label',
-    props: {
-      userData: {
-        type: Object,
-        default: function () {
-          return {
-            _id: '',
-            username: '',
-            avatarUrl: '',
-            name: ''
-          }
+import { mapState } from 'vuex'
+
+export default {
+  name: 'profile-label',
+  props: {
+    userData: {
+      type: Object,
+      default: function () {
+        return {
+          _id: '',
+          username: '',
+          avatarUrl: '',
+          name: ''
         }
-      },
-      showName: {
-        type: Boolean,
-        required: false,
-        default: true
       }
     },
-    computed: {
-      ...mapState({
-        displayName () {
-          return this.userData.name || this.userData.username
-        },
-        avatarUrl (state) {
-          return this.userData.avatarUrl || state.placeholderAvatar
-        }
-      })
+    showName: {
+      type: Boolean,
+      required: false,
+      default: true
+    }
+  },
+  computed: {
+    ...mapState({
+      displayName () {
+        return this.userData.name || this.userData.username
+      },
+      avatarUrl (state) {
+        return this.userData.avatarUrl || state.placeholderAvatar
+      }
+    }),
+    imageStyle () {
+      return `background: url('${this.avatarUrl}') center center / cover no-repeat`
     }
   }
+}
 </script>
 
 <style scoped lang="stylus">
@@ -50,6 +61,7 @@
     max-width 25px
     min-height 25px
     border-radius 50%
+
   .name
     margin-left 12px
 </style>
