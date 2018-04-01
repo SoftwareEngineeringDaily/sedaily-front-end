94a75f4aa0757cfcb38f6dac7dbb69a388b59fee
Josh Morel <morel.josh@hotmail.com>
2018-04-01 10:58:20 -0400

    set-up prerender-node

--- dist/server.js
+++ dist/server.js
@@ -3,6 +3,11 @@ const serveStatic = require('serve-static')
 
 const app = express()
 
+// prerender-node must be used before connect-history-api-fallback
+app.use(require('prerender-node')
+  .set('prerenderToken', process.env.PRERENDER_TOKEN)
+)
+
 app.use(require('connect-history-api-fallback')({
   verbose: true
 }))
