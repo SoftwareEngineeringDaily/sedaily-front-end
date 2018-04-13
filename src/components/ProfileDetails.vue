fae5fa0dca3d433734b501b30a83bd0173054ef9
Tyler Furby <tyler@tylerfurby.com>
2018-04-13 02:12:00 -0600

    Redesign Profile Page
    This will be the first of many design changes to the profile page, here's an initial couple changes.

--- src/components/ProfileDetails.vue
+++ src/components/ProfileDetails.vue
@@ -15,18 +15,19 @@
           <img class="profile-img" :src="avatarUrl" />
         </div>
       </div>
-      <div class="col-sm-6 col-md-4">
-        <h4>
+      <div class="user-details col-sm-6 col-md-4">
+        <h4 class="display-name">
           {{displayName}}
         </h4>
-        <p v-if="userData.website">
+        <p class="display-bio">
+          <small class="text-muted">{{displayBio}}</small>
+        </p>
+        <p class="display-website" v-if="userData.website">
           <a :href="userData.website | externalUrl" target="_blank"
              rel="external nofollow"
           > {{ userData.website | host }} </a>
         </p>
-        <p>
-          <small class="text-muted">{{displayBio}}</small>
-        </p>
+      <hr/>
       </div>
   </div>
 </template>
@@ -72,24 +73,55 @@
 
 <style scoped lang="stylus">
   @import './../css/variables'
-  .profile-details
-    padding-top 5px
+
+  .user-details
+    position fixed
+    padding 0
+    margin 0
+    top 30%
+    left 50%
+    transform: translate(-50%, -50%)
+    text-align center
+    .display-name
+      padding-top 50px
+    .display-website a
+      text-decoration none
+      color primary-color
+      &:hover
+        font-weight bold
+    .display-bio .text-muted
+      font-weight bold
 
   .crop-image
+    position: fixed;
+    top: 20%;
+    left: 50%;
+    transform: translate(-50%, -50%)
     background-position 50%
     background-repeat no-repeat
     border-radius 50%
     width 100px
     height 100px
     overflow hidden
+    transition all .5s ease
+    &:hover
+      width 120px
+      height 120px
 
   .profile-img
     display inline
-    margin 0 auto
+    margin-left 0 auto
     height 100%
     width auto
 
   .edit-link
+    position fixed
+    padding 0
+    margin 0
+    top 39%
+    left 50%
+    transform: translate(-50%, -50%)
+    text-align center
     a
       color accent-color
 </style>
