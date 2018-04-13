961dbf5e5604271af6bc84791c9559a8cc5a81e8
Emily Rosina Carey <emilyrosinacarey@gmail.com>
2018-04-13 17:57:23 +0100

    Tidy up getters

--- src/store/getters.js
+++ src/store/getters.js
@@ -1,39 +1,45 @@
 import { filter, flow, orderBy, values } from 'lodash/fp'
 
 export default {
+  isChatBoxDisplayed: ({ chat }) => {
+    return Boolean(chat.settings.displayBox)
+  },
+  isChatOnline: ({ chat }) => {
+    return Boolean(chat.online)
+  },
+  isPlayerActive: (state) => {
+    return state.activePlayerPost.hasOwnProperty('_id')
+  },
+  getToken (state) {
+    return state.token
+  },
+  isLoggedIn (state, getters) {
+    return Boolean(getters.getToken)
+  },
+  alreadySubscribed (state, getters) {
+    return getters.isLoggedIn && state.me.signedUpForNewsletter
+  },
+
+  // getters with params
   activePosts: (state, getters) => (page = 0) => {
-    // @TODO if page is larger then send new request
+    // TODO: if page is larger then send new request
     const prev = page - 1
     const prevOffset = prev * state.postsPerPage
     const pageOffset = page * state.postsPerPage
-
     return state.lists[state.activeType].slice(prevOffset, pageOffset)
   },
-  isChatBoxDisplayed: ({ chat }) => Boolean(chat.settings.displayBox),
-  isChatOnline: ({ chat }) => Boolean(chat.online),
-  isPlayerActive: ({ activePlayerPost }) => activePlayerPost && activePlayerPost._id,
-  getToken: (state) => state.token,
-  isLoggedIn: (state, getters) => Boolean(getters.getToken),
   getNextEpisode: (state) => (previousPost) => {
     const postsAfterCurrent = flow([
       values,
       filter((post) => post.date < previousPost.date),
       orderBy(['date'], ['desc'])
     ])(state.posts)
-
-    if (postsAfterCurrent.length) {
-      return postsAfterCurrent[0]
-    } else {
-      return state.posts[0]
-    }
+    return postsAfterCurrent.length ? postsAfterCurrent[0] : state.posts[0]
   },
   metaTag: (state) => (vmid, contentValue = undefined) => {
     // use default if contentValue not specified
     const content = contentValue ? contentValue: state.defaultMetaContent[vmid]
     // open graph (og), uses 'property' key instead of 'name'
-    if (vmid.substr(0,2) === 'og') {
-      return { vmid, property: vmid, content}
-    }
-    return { vmid, name: vmid, content }
+    return vmid.substr(0,2) === 'og' ? { vmid, property: vmid, content} : { vmid, name: vmid, content }
   }
 }
