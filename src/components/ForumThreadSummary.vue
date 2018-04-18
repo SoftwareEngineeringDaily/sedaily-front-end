707e1e785cf0e0cc0c8539d7d12b1328eca32b08
Tyler Furby <tyler@tylerfurby.com>
2018-04-18 16:26:25 -0600

    Fix fonts on forum, fix CSS
    The fonts were defaulting to times for users who did not have Roboto-Light installed, also .comments-count-link was invalid, changed this to .forum-thread-misc a, also removed text-decoration

--- src/components/ForumThreadSummary.vue
+++ src/components/ForumThreadSummary.vue
@@ -83,10 +83,10 @@ export default {
 @import '../css/variables'
 
 .forum-summary-container
-  font-family Roboto-Light
+  font-family Roboto
   .forum-summary-title a
     color #000
-    font-weight 300
+    font-weight light
     font-size 1.0rem
   .forum-summary-title a:hover
     text-decoration none
@@ -100,8 +100,13 @@ export default {
   margin-top 3px
   margin-bottom 1rem
 
-.comments-count-link
+.forum-thread-misc a
+  text-decoration none
+
+.misc-detail a
   color #a9a9a9
+  &:hover
+    color idle-foreground
 .bullet-point
   display inline-flex
   font-size 0.65em
