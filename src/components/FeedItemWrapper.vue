d7520d6cd17917d8338591130d5025dc340b4aa5
jason <bjason@gmail.com>
2018-05-14 16:16:59 -0700

    Render forum item

--- src/components/FeedItemWrapper.vue
+++ src/components/FeedItemWrapper.vue
@@ -1,11 +1,34 @@
 <template>
   <div>
-    Feed Item
+    <forum-thread-summary
+    v-if="isThread"
+    :key="feedItem._id"
+    :forumThread="feedItem"/>
+    <div v-else>
+      Feed Item
+    </div>
+
   </div>
 </template>
 
 <script>
+  import FeedItem from '@/components/FeedItem.vue'
+  import ForumThreadSummary from '@/components/ForumThreadSummary'
   export default {
-    name: 'feed-item-wrapper'
+    name: 'feed-item-wrapper',
+    props: {
+      feedItem: {
+        type: Object,
+        required: true
+      }
+    },
+    components: {
+      ForumThreadSummary
+    },
+    computed: {
+      isThread () {
+        return this.feedItem.relatedLink == null;
+      }
+    }
   }
 </script>
