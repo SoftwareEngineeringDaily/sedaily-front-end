d848651d701b20709df16da6120e76aa313b3df9
Josh Morel <morel.josh@hotmail.com>
2018-06-16 10:50:18 -0400

    cypress: add more tests to profile spec

--- cypress/integration/profile_spec.js
+++ cypress/integration/profile_spec.js
@@ -1,3 +1,6 @@
+const uuidv4 = require('uuid/v4')
+
+
 describe('End-to-End: The Profile Page', function () {
   let existingUser
   before(function () {
@@ -8,7 +11,9 @@ describe('End-to-End: The Profile Page', function () {
     })
   })
   it('Successfully displays and updates profile', function () {
-    let newWebsite = 'www.me.com'
+    const newWebsite = 'www.me.com'
+    const newBio = 'My Bio!'
+    const newName = uuidv4()
     cy.visit('/')
     .then(() => {
       cy.get('a[href="/profile"]').click()
@@ -20,13 +25,20 @@ describe('End-to-End: The Profile Page', function () {
     cy.location().should((loc) => {
       expect(loc.pathname).to.contain('edit')
     })
+    // populates with existing
     cy.get('input[name=username]').should('have.value', existingUser.username)
     cy.get('input[name=name]').should('have.value', existingUser.name)
     cy.get('input[name=email]').should('have.value', existingUser.email)
+    // input and update
     cy.get('#websiteInput').type(newWebsite)
+    cy.get('#bioInput').type(newBio)
+    cy.get('input[name=name]').type(newName)
     cy.contains('Update').click()
     cy.contains('was Updated').should('exist')
+    // check updated
     cy.get('a[href="/profile"]').click()
     cy.get(`a[href="//${newWebsite}"]`).should('exist')
+    cy.get('.display-bio > small').should('contain', newBio)
+    cy.get('.display-name').should('contain', newName)
   })
 })
