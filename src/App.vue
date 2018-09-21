53ad1dca8991e32dac485f132126748bef5a0a77
Andrew Lloyd <andrew85.lloyd@gmail.com>
2018-09-20 20:58:24 -0700

    Add notice to users about not being able to post. Temporarily disable forum e2e tests (#549)

--- src/App.vue
+++ src/App.vue
@@ -17,8 +17,14 @@
             to="/contributors"
             class="link">contributor</router-link>.
         </div>
-      </div>
 
+      </div>
+      <div class="row">
+        <div
+          class="col-md-12 beta-msg text-center">
+            <b>Notice: </b><span>We have temporarily disabled posting threads while we implement Captcha. Thank you for your patience and we apologize for the inconvenience.</span>
+          </div>
+      </div>
       <div class="row">
         <transition
           name="fade"
