14837a0407e58b7f9bb3611a0def9bae3ba381d3
Arkadiusz Celer <arku@codepany.com>
2019-03-13 13:10:44 +0100

    changing dev config

--- config/apiConfig/dev.api.config.js
+++ config/apiConfig/dev.api.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-	BASE_URL: 'http://217.182.74.142:4040/api',
+	BASE_URL: 'http://localhost:4040/api',
 	STRIPE_PUBLIC_KEY: 'pk_test_vMQkcW5KspBBOQ0hn5nNshDm',
   EVENTS_API_BASE_URL: 'http://localhost:3000/api/v1/event'
 }
