66fd9f1409203cba18ff75e6f8460d1b4810c242
Andrew Lloyd <andrew85.lloyd@gmail.com>
2019-06-12 20:08:17 -0700

    use quotes for docker password

--- devops/build-image.sh
+++ devops/build-image.sh
@@ -20,7 +20,7 @@ docker build -f $REPO_DIR/ci.Dockerfile -t $DOCKER_IMAGE $REPO_DIR #--no-cache
 rm $REPO_DIR/ci.Dockerfile
 
 # must be part the organization
-echo $DOCKER_PASSWORD | docker login -u "$DOCKER_USERNAME" --password-stdin
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
 docker push $DOCKER_IMAGE
 
