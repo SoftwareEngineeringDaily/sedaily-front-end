b4058eb930ceb249da891b841ab86f04ba2de7c5
sterlingrules <me@mynameissterling.com>
2020-05-06 11:30:18 -0400

    fix(): incorrect twitter url when switching posts - fixes #792

--- src/views/post/PostView.vue
+++ src/views/post/PostView.vue
@@ -162,6 +162,7 @@ export default {
       showPostContent: true,
       comment: '',
       highlight: '',
+      contentUrl: window.location.href,
       isLoadingComments: false,
       loading: false,
     }
@@ -177,13 +178,13 @@ export default {
 
   computed: {
     forumThreadId () {
-      if (!this.isLoggedIn) return '' // Expects a string
-      if (!(this.post && this.post.thread)) return '' // Expects a string
-      return this.post.thread._id
-    },
+      const hasThread = (this.post && this.post.thread)
 
-    contentUrl () {
-      return window.location.href
+      if (!this.isLoggedIn || !hasThread) {
+        return '' // Expects a string
+      }
+
+      return this.post.thread._id
     },
 
     postContent () {
@@ -383,6 +384,7 @@ export default {
       const post_keys = Object.keys(this.$store.state.posts)
 
       this.loading = false
+      this.contentUrl = window.location.href
 
       // Fetch comments
       if (post.thread) {
