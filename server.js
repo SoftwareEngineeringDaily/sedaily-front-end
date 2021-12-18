47ab915d4d9b12d7405165e2d9add3f895e093d6
Tarek Ali <tarekali15@outlook.com>
2021-12-17 22:38:20 -0600

    changed the way prerender is imported

--- server.js
+++ server.js
@@ -2,11 +2,10 @@ const sslRedirect = require('heroku-ssl-redirect')
 const express = require('express')
 const serveStatic = require('serve-static')
 const app = express()
+const prerender = require('prerender-node')
 
 // prerender-node must be used before connect-history-api-fallback
-app.use(require('prerender-node')
-  .set('prerenderToken', process.env.PRERENDER_TOKEN)
-)
+app.use(prerender.set('prerenderToken', process.env.PRERENDER_TOKEN))
 
 // enable ssl redirect
 app.use(sslRedirect())
