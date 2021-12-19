1b002581a0f1828d2b20e59c90d8c60f49336128
Tarek Ali <tarekali15@outlook.com>
2021-12-19 11:30:38 -0600

    method hierarchy

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
 const sslRedirect = require('heroku-ssl-redirect')
 const express = require('express')
 const serveStatic = require('serve-static')
-const app = express()
 const prerender = require('prerender-node')
-
-// prerender-node must be used before connect-history-api-fallback
-app.use(prerender.set('prerenderToken', process.env.PRERENDER_TOKEN))
+const app = express()
 
 // enable ssl redirect
 app.use(sslRedirect())
 
+// prerender-node must be used before connect-history-api-fallback
+app.use(prerender.set('prerenderToken', process.env.PRERENDER_TOKEN))
+
 const apiUrl = 'https://software-enginnering-daily-api.herokuapp.com/api'
+
 app.get('/rss/public', (_, res) => res.redirect(`${apiUrl}/rss/public/all`))
 app.get('/rss/unlimited', (_, res) => res.redirect(`${apiUrl}/rss/public/all_unlimited`))
 app.get('/rss/private/:id', (req, res) => res.redirect(`${apiUrl}/rss/private/${req.params.id}`))
