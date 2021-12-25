d8fa5189445409a89e226e2492297917d331f7a4
Tarek Ali <tarekali15@outlook.com>
2021-12-24 22:02:13 -0600

    changed position of topic link, def will need to change styles

--- src/views/job/JobView.vue
+++ src/views/job/JobView.vue
@@ -6,22 +6,34 @@
           <spinner :show="loading"></spinner>
         </div>
         <div v-else-if="error">
-          <div class="bg-danger"> Error: {{ error }}</div>
+          <div class="bg-danger">Error: {{ error }}</div>
         </div>
         <div v-else-if="job && job.isDeleted">
-          <div class="bg-warning"> You previously deleted the job: {{ job.title }}</div>
+          <div class="bg-warning">
+            You previously deleted the job: {{ job.title }}
+          </div>
         </div>
         <div v-else-if="job" class="col-md-10 offset-md-1">
           <h4 class="row">{{ job.title }} - {{ job.employmentType }}</h4>
-          <div class="row">{{ job.companyName }} - {{ job.location }}
+          <div class="row">
+            {{ job.companyName }} - {{ job.location }}
             <span v-if="job.remoteWorkingConsidered">&nbsp;(Remote Ok)</span>
           </div>
-          <br>
+          <div class="row">
+            <router-link
+              v-if="job.topics && job.topics.length"
+              v-for="topic in job.topics"
+              :to="`/topic/${topic.slug}`"
+              :key="topic._id"
+              class="topic"
+            >
+              #{{ topic.name }}
+            </router-link>
+          </div>
+          <br />
           <div class="row job-link">
             <span v-if="!isLoggedIn">
-              <router-link to="/login">
-                Login to Apply
-              </router-link>
+              <router-link to="/login"> Login to Apply </router-link>
             </span>
             <span v-else-if="ownJobPosting">
               <router-link :to="'/edit-job/' + jobId">
@@ -29,41 +41,25 @@
               </router-link>
             </span>
             <span v-else>
-
               <job-apply-modal
                 v-if="job"
                 :id="'jobApplyModal'"
                 :title="job.title"
                 :isModalVisible="isModalVisible"
                 :onClose="toggleJobApplyModal"
-                :jobId="jobId">
-
-                <button
-                  class="btn button-submit"
-                  @click="toggleJobApplyModal">
+                :jobId="jobId"
+              >
+                <button class="btn button-submit" @click="toggleJobApplyModal">
                   Apply to Job
                 </button>
-
               </job-apply-modal>
-
             </span>
           </div>
           <div class="row job-description">
-              {{ job.description }}
+            {{ job.description }}
           </div>
-          <div class="row job-date">
-            Posted on {{ date }}
-            <span v-if="job.topics && job.topics.length" class="divider">|</span>
-            <router-link
-              v-if="job.topics && job.topics.length"
-              v-for="topic in job.topics"
-              :to="`/topic/${topic.slug}`"
-              :key="topic._id"
-              class="topic">
-              #{{topic.name}}
-            </router-link>
-          </div>
-          <br>
+          <div class="row job-date">Posted on {{ date }}</div>
+          <br />
         </div>
       </div>
     </div>
@@ -71,130 +67,137 @@
 </template>
 
 <script>
-import moment from 'moment'
-import Spinner from '@/components/Spinner'
-import JobApplyModal from '@/components/job/JobApplyModal'
-import { mapActions, mapState, mapGetters } from 'vuex'
+import moment from "moment";
+import Spinner from "@/components/Spinner";
+import JobApplyModal from "@/components/job/JobApplyModal";
+import { mapActions, mapState, mapGetters } from "vuex";
 export default {
-  name: 'job-view',
+  name: "job-view",
 
-  data () {
+  data() {
     return {
       loading: false,
       job: null,
       isModalVisible: false,
-      error: null
-    }
+      error: null,
+    };
   },
 
-  created () {
-    this.fetchData()
+  created() {
+    this.fetchData();
   },
 
   watch: {
     // re-fetch if route changes
-    '$route': 'fetchData'
+    $route: "fetchData",
   },
 
   components: {
     Spinner,
-    JobApplyModal
+    JobApplyModal,
   },
 
   methods: {
     // TODO: once profile issue resolved, don't fetch profile here
     // https://github.com/SoftwareEngineeringDaily/sedaily-front-end/issues/239
-    ...mapActions(['fetchJob']),
+    ...mapActions(["fetchJob"]),
 
-    toggleJobApplyModal () {
-      this.isModalVisible = !this.isModalVisible
+    toggleJobApplyModal() {
+      this.isModalVisible = !this.isModalVisible;
     },
 
-    fetchData () {
-      this.loading = true
+    fetchData() {
+      this.loading = true;
       this.fetchJob({ jobId: this.jobId })
         .then((response) => {
-          this.job = response.data
-          this.error = null
+          this.job = response.data;
+          this.error = null;
         })
         .catch((error) => {
-          this.error = error.response.data.message
+          this.error = error.response.data.message;
         })
         .finally(() => {
-          this.loading = false
-        })
-    }
+          this.loading = false;
+        });
+    },
   },
 
   computed: {
-    ...mapGetters(['isLoggedIn', 'metaTag']),
+    ...mapGetters(["isLoggedIn", "metaTag"]),
 
     ...mapState({
-      jobId (state) {
-        return state.route.params.id
+      jobId(state) {
+        return state.route.params.id;
       },
-      me (state) {
-        return state.me
+      me(state) {
+        return state.me;
       },
-      ownJobPosting () {
-        return this.me._id === this.job.postedUser
+      ownJobPosting() {
+        return this.me._id === this.job.postedUser;
       },
-      date () {
-        return moment(this.job.postedDate).format('MMMM Do, YYYY')
-      }
+      date() {
+        return moment(this.job.postedDate).format("MMMM Do, YYYY");
+      },
     }),
 
     metaDescription() {
       const maxLength = 400;
-      const { job: { description } } = this
+      const {
+        job: { description },
+      } = this;
       if (description.length > maxLength) {
-        return description.substr(0, maxLength-3) + '...'
+        return description.substr(0, maxLength - 3) + "...";
       }
-      return description
-    }
+      return description;
+    },
   },
 
   metaInfo() {
     // wait for job before updating meta
     if (!this.job) {
-      return {}
+      return {};
     }
 
-    const title = `${this.job.title} | Software Daily`
-    const { metaDescription } = this
+    const title = `${this.job.title} | Software Daily`;
+    const { metaDescription } = this;
 
     return {
       title,
       meta: [
-        this.metaTag('og:title', title),
-        this.metaTag('og:url', location.href),
-        this.metaTag('description', metaDescription),
-        this.metaTag('og:description', metaDescription)
-      ]
-    }
+        this.metaTag("og:title", title),
+        this.metaTag("og:url", location.href),
+        this.metaTag("description", metaDescription),
+        this.metaTag("og:description", metaDescription),
+      ],
+    };
   },
-}
+};
 </script>
 
 <style scoped lang="stylus">
-@import '../../css/variables'
+@import '../../css/variables';
 
-.job-link
-  a
-    color accent-color
+.job-link {
+  a {
+    color: accent-color;
+  }
+}
 
-.job-description
+.job-description {
   white-space: normal;
+}
 
-.job-date
-  font-size 0.9em
-  color #999
+.job-date {
+  font-size: 0.9em;
+  color: #999;
+}
 
-.divider
-  margin 0 12px
+.divider {
+  margin: 0 12px;
+}
 
-.topic
-  margin-right 6px
-  color #999
-
+.topic {
+  margin-right: 6px;
+  color: #999;
+}
 </style>
