2b7b83438f6c3cd121afaa4c2baf3011aa186616
Tarek Ali <tarekali15@outlook.com>
2021-12-26 12:08:06 -0600

    hyperlink to login page when logged in (instead of using a toast), and formatting of this file

--- src/components/topic/TopicCreate.vue
+++ src/components/topic/TopicCreate.vue
@@ -1,174 +1,178 @@
 <template>
   <div class="topicpage-create">
-
-    <button
-      class="link-button"
-      @click="toggleModal">
-      <i class="fa fa-plus" /> Post 
+    <button class="link-button" @click="toggleModal">
+      <i class="fa fa-plus" /> Post
     </button>
 
-    <modal
-      v-show="isModalVisible">
-      <h4 slot="header">
-        What is the name of your project or company?
-      </h4>
+    <modal v-show="isModalVisible">
+      <h4 slot="header">What is the name of your project or company?</h4>
       <template slot="body">
-        <p>We are always looking for new show ideas to cover in Software Daily. Here you can submit a company or project idea.</p>
+        <p>
+          We are always looking for new show ideas to cover in Software Daily.
+          Here you can submit a company or project idea.
+        </p>
         <b-input
           autocomplete="off"
           :autofocus="true"
           @keyup.enter="onSubmit"
           placeholder="Enter project or company name"
-          v-model="newTopicText" />
+          v-model="newTopicText"
+        />
       </template>
       <template slot="footer">
         <button type="button" class="btn-modal-secondary" @click="toggleModal">
           Cancel
         </button>
         <button type="button" class="button-submit" @click="onSubmit">
-          <spinner :show="saving"/>
+          <spinner :show="saving" />
           Submit
         </button>
       </template>
     </modal>
-
   </div>
 </template>
 
 <script>
-import { mapState, mapGetters, mapActions } from 'vuex'
-import Spinner from "@/components/Spinner.vue"
-import modal from '@/components/ModalComponent'
+import { mapState, mapGetters, mapActions } from "vuex";
+import Spinner from "@/components/Spinner.vue";
+import modal from "@/components/ModalComponent";
 
 export default {
-  name: 'topicpage-create',
+  name: "topicpage-create",
 
   components: {
     Spinner,
-    modal
+    modal,
   },
 
-  data () {
+  data() {
     return {
-        saving: false,
-        isModalVisible: false,
-        newTopicText: '',
-    }
+      saving: false,
+      isModalVisible: false,
+      newTopicText: "",
+    };
   },
   computed: {
-    ...mapGetters([
-      'isLoggedIn',
-    ]),
+    ...mapGetters(["isLoggedIn"]),
 
     ...mapState({
       me(state) {
-        return state.me
-      }
-    })
+        return state.me;
+      },
+    }),
   },
   methods: {
-    ...mapActions([
-      'createTopic',
-    ]),
+    ...mapActions(["createTopic"]),
 
-    toggleModal () {
+    toggleModal() {
       if (!this.isModalVisible && !this.isLoggedIn) {
-        return this.$toasted.error('Please login.', { duration: 5000 })
+        this.$router.push("/login");
       }
 
-      this.isModalVisible = !this.isModalVisible
+      this.isModalVisible = !this.isModalVisible;
     },
 
-    async onSubmit () {
-      const name = this.newTopicText.trim()
+    async onSubmit() {
+      const name = this.newTopicText.trim();
 
       if (!name || !this.me._id) {
-        return
+        return;
       }
 
       const data = {
         name,
         maintainer: this.me._id,
         isUserGenerated: true,
-      }
+      };
 
-      this.saving = true
+      this.saving = true;
 
       try {
-        const saved = await this.createTopic({ data })
+        const saved = await this.createTopic({ data });
 
         this.$router.push({
-          name: 'TopicPageEdit',
-          params: { slug: saved.slug }
-        })
-      }
-      catch (e) {
-        this.$toasted.error((e.response) ? e.response.data : e, { duration : 0 })
+          name: "TopicPageEdit",
+          params: { slug: saved.slug },
+        });
+      } catch (e) {
+        this.$toasted.error(e.response ? e.response.data : e, { duration: 0 });
       }
 
-      this.saving = false
+      this.saving = false;
     },
-  }
-}
+  },
+};
 </script>
 
 <style lang="stylus" scoped>
-@import '~@/css/variables'
+@import '~@/css/variables';
 
-.link-button
-  cursor pointer
-  display inline-block
-  padding 4px 12px
-  font-size 14px
-  font-weight 700
-  color #a591ff
-  border 2px solid #a591ff
-  border-radius 18px
-  background-color #ffffff
+.link-button {
+  cursor: pointer;
+  display: inline-block;
+  padding: 4px 12px;
+  font-size: 14px;
+  font-weight: 700;
+  color: #a591ff;
+  border: 2px solid #a591ff;
+  border-radius: 18px;
+  background-color: #ffffff;
 
-  &:hover
-    text-decoration none
-    color #ffffff
-    background-color #a591ff
+  &:hover {
+    text-decoration: none;
+    color: #ffffff;
+    background-color: #a591ff;
+  }
+}
 
-.topicpage-create
-  h3
-    text-align center
-    margin 40px 0
+.topicpage-create {
+  h3 {
+    text-align: center;
+    margin: 40px 0;
+  }
 
-  .button-submit
-    .spinner
-      width 18px
-      height 18px
+  .button-submit {
+    .spinner {
+      width: 18px;
+      height: 18px;
 
-      >>> circle
-        stroke #ffffff
+      >>> circle {
+        stroke: #ffffff;
+      }
+    }
+  }
 
-  >>> .modal
-    height auto
-    min-height 0
+  >>> .modal {
+    height: auto;
+    min-height: 0;
 
-    .modal-body
-      padding 15px
-      display flex
-      flex-direction column
-      min-height 0
+    .modal-body {
+      padding: 15px;
+      display: flex;
+      flex-direction: column;
+      min-height: 0;
+    }
 
-    input
-      outline none
-      font-size 14px
+    input {
+      outline: none;
+      font-size: 14px;
 
-      &:focus
-        outline none
-        border 0
-        -webkit-box-shadow 0 0 3px 3px rgba(165,145,255,0.4)
-        box-shadow 0 0 3px 3px rgba(165,145,255,0.4)
+      &:focus {
+        outline: none;
+        border: 0;
+        -webkit-box-shadow: 0 0 3px 3px rgba(165, 145, 255, 0.4);
+        box-shadow: 0 0 3px 3px rgba(165, 145, 255, 0.4);
+      }
+    }
 
-    .btn-modal-secondary
-      background-color none
-      color #222
-      margin-right 5px
-      padding 5px
-      border none
-      outline none
+    .btn-modal-secondary {
+      background-color: none;
+      color: #222;
+      margin-right: 5px;
+      padding: 5px;
+      border: none;
+      outline: none;
+    }
+  }
+}
 </style>
